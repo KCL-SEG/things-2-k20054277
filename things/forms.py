077a433f5e29aa0b87e05d1363e221998ed84f71
Jiaqi Xu <k20054277@kcl.ac.uk>
2022-12-07 21:12:18 +0000

    test to work on server

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,27 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from .models import Thing
+
+class ThingForm(forms.Form):
+	
+    class Meta:
+        model = Thing
+        fields=["name","description","quantity"]
+
+    name=forms.CharField(
+        label="name",
+    )
+
+    description=forms.CharField(
+        label="description",
+        widget = forms.Textarea(),
+    )
+
+    quantity=forms.IntegerField(
+        widget = forms.NumberInput(),
+        label="quantity",
+    )
+
+
--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    template_name = 'home.html'
+    form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
